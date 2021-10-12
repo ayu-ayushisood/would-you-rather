afc9a9c9b379f264111a8ab0b4a8db74a14b98c0
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-10-12 18:54:01 +0530

    add api file in utils

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+  } from "./_DATA";
+  
+  export function getInitialData() {
+    return Promise.all([_getUsers(), _getQuestions()]).then(
+      ([users, questions]) => ({
+        users,
+        questions
+      })
+    );
+  }
+  
+  export function saveQuestion(question) {
+    return Promise.all([_saveQuestion(question)]).then(([question]) => question);
+  }
+  
+  export function saveQuestionAnswer(info) {
+    return _saveQuestionAnswer(info);
+  }
