eb108322d36ec2ee643e625bad989dfda3a1ab23
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-10-19 20:37:35 +0530

    add react bootstrap

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import middlewares from './middlewares';
 import reducer from './reducers';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(reducer, middlewares);
 
@@ -16,8 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
